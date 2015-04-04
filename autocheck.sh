be0419145054f5688c00690e26a30c4bca69133d
Kai-Zhang <cs.zhangkai@outlook.com>
2015-04-04 13:22:32 +0800

    Build testcases in src/testcase.c autocheck.sh to autorun the qemu to check the producer and consumer

---
+++ autocheck.sh
@@ -0,0 +1,42 @@
+logfile=log
+maxrun=0	# Run qemu for $maxrun times. When this value is 0, qemu will run forever (use ctrl+C to stop running).
+timeinterval=5	# Each run lasts for $timeinterval second.
+i=1
+ 
+function die {
+	pkill -9 qemu
+	echo -e $1 1>&2
+	stty echo
+	exit
+}
+ 
+make disk.img
+if [ $? != 0 ]
+then
+	exit
+fi
+ 
+while true
+do
+	echo -e "\nstart the ${i}th test..."
+ 
+	qemu-system-i386 -serial stdio disk.img 2>&1 | tee $logfile &
+	sleep $timeinterval
+ 
+	if grep -q 'system panic' $logfile
+	then
+		die '\n\033[1;31mSystem panic is triggered!\033[0m'
+	elif grep -q 'qemu: fatal:' $logfile
+	then
+		die '\n\033[1;31mqemu has crashed!\033[0m'
+	elif [ `grep -c 'Hello, OS World!' $logfile` != 1 ]
+	then
+		die '\n\033[1;31mMysterious reboot is detected!\033[0m'
+	elif [ $i == $maxrun ]
+	then
+	    	die '\n\033[1;31mReach maximal run!\033[0m'
+	fi
+ 
+	pkill -9 qemu
+	i=$((i + 1))
+done
---
+++ src/testcase.c
@@ -0,0 +1,228 @@
+#include "kernel.h"
+#define NBUF 5
+#define NR_PROD 3
+#define NR_CONS 4
+ 
+int buf[NBUF];
+volatile int f = 0, r = 0, g = 1;
+int last = 0;
+Semaphore empty, full, mutex;
+ 
+// Accelerate the time clock to expose the problem of critical area
+void
+accelerate(void) {
+	#define PORT_TIME 0x40
+	#define FREQ_8253 1193182
+	#define HZ        10000
+	 
+	int count = FREQ_8253 / HZ;
+	assert(count < 65536);
+	out_byte(PORT_TIME + 3, 0x34);
+	out_byte(PORT_TIME    , count % 256);
+	out_byte(PORT_TIME    , count / 256);
+}
+
+
+// --------------------------------------------------
+// Test Semaphore - producer and consumer
+// --------------------------------------------------
+void
+producer(void) {
+	while (1) {
+		P(&empty);
+		P(&mutex);
+		printk(".");	// tell us threads are really working
+		buf[f ++] = g ++;
+		f %= NBUF;
+		V(&mutex);
+		V(&full);
+	}
+}
+ 
+void
+consumer(void) {
+	int get;
+	while (1) {
+		P(&full);
+		P(&mutex);
+		get = buf[r ++];
+		assert(last == get - 1);	// the products should be strictly increasing
+		last = get;
+		r %= NBUF;
+		V(&mutex);
+		V(&empty);
+	}
+}
+ 
+void
+test_semaphore(void) {
+	init_sem(&full, 0);
+	init_sem(&empty, NBUF);
+	init_sem(&mutex, 1);
+	int i;
+	for(i = 0; i < NR_PROD; i ++) {
+		wakeup(create_kthread(producer));
+	}
+	for(i = 0; i < NR_CONS; i ++) {
+		wakeup(create_kthread(consumer));
+	}
+}
+
+
+// --------------------------------------------------
+// Test kthread create and schedule
+// --------------------------------------------------
+void
+kthread_A (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {printk("a");}
+        x ++;
+    }
+}
+
+void
+kthread_B (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {printk("b");}
+        x ++;
+    }
+}
+
+void
+test_basic_kthread(void) {
+	create_kthread(kthread_A);
+	create_kthread(kthread_B);
+}
+
+
+// --------------------------------------------------
+// Test printk function
+// --------------------------------------------------
+void
+test_printk(void) {
+	printk("Printk test begin...\n");
+	printk("the answer should be:\n");
+	printk("#######################################################\n");
+	printk("Hello, welcome to OSlab! I'm the body of the game. ");
+	printk("Bootblock loads me to the memory position of 0x100000, and Makefile also tells me that I'm at the location of 0x100000. ");
+	printk("~!@#$^&*()_+`1234567890-=...... ");
+	printk("Now I will test your printk: ");
+	printk("1 + 1 = 2, 123 * 456 = 56088\n0, -1, -2147483648, -1412505855, -32768, 102030\n0, ffffffff, 80000000, abcdef01, ffff8000, 18e8e\n");
+	printk("#######################################################\n");
+	printk("your answer:\n");
+	printk("=======================================================\n");
+	printk("%s %s%scome %co%s", "Hello,", "", "wel", 't', " ");
+	printk("%c%c%c%c%c! ", 'O', 'S', 'l', 'a', 'b');
+	printk("I'm the %s of %s. %s 0x%x, %s 0x%x. ", "body", "the game", "Bootblock loads me to the memory position of", 
+		0x100000, "and Makefile also tells me that I'm at the location of", 0x100000);
+	printk("~!@#$^&*()_+`1234567890-=...... ");
+	printk("Now I will test your printk: ");
+	printk("%d + %d = %d, %d * %d = %d\n", 1, 1, 1 + 1, 123, 456, 123 * 456);
+	printk("%d, %d, %d, %d, %d, %d\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
+	printk("%x, %x, %x, %x, %x, %x\n", 0, 0xffffffff, 0x80000000, 0xabcedf01, -32768, 102030);
+	printk("=======================================================\n");
+	printk("Test end!!! Good luck!!!\n");
+}
+
+
+// --------------------------------------------------
+// Test stack overflow
+// --------------------------------------------------
+void
+stackoverflow(int x) {
+    if(x == 0)
+        printk("%d ",x);
+    if(x > 0)
+        stackoverflow(x - 1);
+}
+
+void
+test_stackoverflow(void) {
+    while(1) {
+        stackoverflow(16384*1000);
+    }
+}
+
+
+// --------------------------------------------------
+// Test kthread sleep and wakeup
+// --------------------------------------------------
+static PCB *PCB_of_thread_A;
+static PCB *PCB_of_thread_B;
+static PCB *PCB_of_thread_C;
+static PCB *PCB_of_thread_D;
+
+void
+A (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {
+            printk("a");
+            wakeup(PCB_of_thread_B);
+            sleep();
+        }
+        x ++;
+    }
+}
+
+void
+B (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {
+            printk("b");
+            wakeup(PCB_of_thread_C);
+            sleep();
+        }
+        x ++;
+    }
+}
+
+void
+C (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {
+            printk("c");
+            wakeup(PCB_of_thread_D);
+            sleep();
+        }
+        x ++;
+    }
+}
+
+void
+D (void) { 
+    int x = 0;
+    while(1) {
+        if(x % 100000 == 0) {
+            printk("d");
+            wakeup(PCB_of_thread_A);
+            sleep();
+        }
+        x ++;
+    }
+}
+
+void
+test_sleep_wakeup(void) {
+	PCB_of_thread_A = create_kthread(A);
+	PCB_of_thread_B = create_kthread(B);
+	PCB_of_thread_C = create_kthread(C);
+	PCB_of_thread_D = create_kthread(D);
+}
+
+// Main entry of testcases
+void
+test_setup(void) {
+	// Select testcases here.
+
+	// accelerate();
+	// test_printk();
+	// test_basic_kthread();
+	// test_stackoverflow();
+	// test_sleep_wakeup();
+	// test_semaphore();
+}
